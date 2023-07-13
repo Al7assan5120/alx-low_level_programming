ad6cc7d914bcb9c3f2a90b8bc08861fe210eec8a
Al7assan <al7assan.r@gmail.com>
2023-07-13 14:06:50 -0700

    c4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - puts a given phrase
+ *Return: always 0
+ */
 int  main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
