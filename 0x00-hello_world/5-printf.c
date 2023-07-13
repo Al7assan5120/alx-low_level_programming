806ddec39667999dd48a0ea6bac609f6ab4d64d7
Al7assan <al7assan.r@gmail.com>
2023-07-13 14:03:19 -0700

    c5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ *main - prints a given phrase
+ *Return: always 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
