730ac70d7d2d102bb857078f52d542ab45094d67
Al7assan <al7assan.r@gmail.com>
2023-07-15 04:18:28 +0300

    c 0.6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n<0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
@@ -22,6 +22,5 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	
 	return (0);
 }
