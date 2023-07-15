78fc03426d2dcb0b44d88d68f91221766f6dd593
Al7assan <al7assan.r@gmail.com>
2023-07-15 06:20:20 +0300

    c 6.0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main-print single digit numbers
+*
+* Return: always return zero
+*/
+int main(void)
+{
+	int x;
+
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
