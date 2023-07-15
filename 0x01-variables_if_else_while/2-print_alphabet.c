ee0243c6c189e2eec68ed0c4859511a1f72c6895
Al7assan <al7assan.r@gmail.com>
2023-07-15 05:25:19 +0300

    c 2.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main-print alphabet
+*
+* Return: always return zero
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
