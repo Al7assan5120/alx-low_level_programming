f41903cffe43a9d51a2c34dccb861353f8a3fe0a
Al7assan <al7assan.r@gmail.com>
2023-07-17 20:07:28 +0300

    c 102.5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,45 @@ int main(void)
 	int y;
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = '0'; j < '9'; j++)
 		{
  			for (x = '0' ; x <= '9' ; x++)
 			{
 				for (y = '0' ; y <= '9'; y++)
 				{
-					if (y == '9' && x == '9')
+					if (y == '0' && x == '0' && j == '0' && i == '0')
 					{
-						y = j+2;
-						x = x;
-						j = j+1;
-						i = i;
-					}
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(x);
-						putchar(y);
-						if (y >= '9' && x >= '9' && j >= '8')
+						continue;
+					}										
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(x);
+					putchar(y);										
+					if (y == '9' && x == '9' && j == '8' && i == '9')
+						{
+							putchar('\n');
+							return (0);
+						}
+					if (y == '9' && x == '9')
+						{
+							i = i;
+							j = j+1;
+							x = x;
+							y = j;
+						}
+					if (y >= '9' && x >= '9' && j >= '8' && i >= '9')
 						{
 							continue;
 						}
-						else
+					else
 						{
 							putchar(',');
 							putchar(' ');
 						}
 				}
 			}
-		}
-		
+		}		
 	}
-	putchar('\n');
 	return (0);
 }
