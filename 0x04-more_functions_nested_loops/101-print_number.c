f82d934e28bccd95d3c7b43163c94bf05b247375
Al7assan <al7assan.r@gmail.com>
2023-07-21 20:37:42 +0300

    c 9.0

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_number - prints an integer.
+*@n: int number
+*Return: no return
+**/
+void print_number(int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if ( n >= 1000)
+        _putchar()
+    }
+    
+}
