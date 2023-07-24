a895ce6b019f0fc404e4deb25d2275cc76d89907
Al7assan <al7assan.r@gmail.com>
2023-07-25 02:37:40 +0300

    c 4.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+*print_rev - prints a string, in reverse.
+*@s: char pointer
+*Return: no return
+**/
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
