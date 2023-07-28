8b84c6e473d39c874a331fa6c1a932780ad66ed1
Al7assan <al7assan.r@gmail.com>
2023-07-28 06:00:09 +0300

    c 3.1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-while ((s1[i] - s2[i]) == 0 && s1[i] != '\0')
-{
-    i++;
-}
-return (s1[i] - s2[i]);
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 }
