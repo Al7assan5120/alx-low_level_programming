a2c5a278d1cd9ab92b1251a56d1cae08787c8a77
Al7assan <al7assan.r@gmail.com>
2023-08-06 22:09:30 +0300

    c 5.5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,41 @@
 #include "main.h"
 /**
- * sqrt - returns the value of x raised to the power of y.
+ * _sqrt - returns the value of x raised to the power of y.
  * @n: the int number
- * @n: the int number
+ * @x: the int number
  * Return: int value of power.
  */
-int sqrt(int n, int x)
+int _sqrt(int n, int x)
 {
-	if (x == 0)
+	if (n == 1)
 	{
-		return (1);
+		return (x);
 	}
-	else if (x < 0)
+	if (x >= n / 2)
 	{
 		return (-1);
 	}
+	if (x * x == n)
+	{
+		return (x);
+	}
 	else
 	{
-        if (x * x != n)
-		{
-            _sqrt_recursion(n, x + 1);
-        }
+		return (_sqrt(n, x + 1));
 	}
-    return (x);
 }
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y.
+ * _sqrt_recursion - returns the value of x raised to the power of y.
  * @n: the int number
  *
  * Return: int value of power.
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt(n, 1));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt(n, 1));
 }
