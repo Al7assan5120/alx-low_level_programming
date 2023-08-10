ea972e4bb760b36d004348b2a39f1c26b833385f
Al7assan <al7assan.r@gmail.com>
2023-08-10 23:40:52 +0300

    c 0.0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - that allocates memory using malloc.
+ * @b: the int value of allocated length
+ *
+ * Return: no return.
+ */
+void *malloc_checked(unsigned int b)
+{
+    void * p;
+
+    p = malloc(b);
+    if (p == NULL)
+    {
+        exit (98);
+    }
+    return (p);
+}
