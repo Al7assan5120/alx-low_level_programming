4558760c0ecc16e3f038df3e8ee9a174bdefca3a
Al7assan <al7assan.r@gmail.com>
2023-08-15 21:49:01 +0300

    c 4.2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,5 +12,38 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-return (new_dog);
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	if (name != NULL)
+	{
+		p->name = malloc((strlen(name) + 1) * sizeof(char));
+		if (p->name == NULL)
+		{
+			free(p);
+			return (NULL);
+		}
+		else
+			strcpy(p->name, name);
+	}
+	else
+		return (NULL);
+	p->age = age;
+	if (owner != NULL)
+	{
+		p->owner = malloc((strlen(owner) + 1) * sizeof(char));
+		if (p->owner == NULL)
+		{
+			free(p->name);
+			free(p);
+			return (NULL);
+		}
+		else
+			strcpy(p->owner, owner);
+	}
+	else
+		return (NULL);
+return (p);
 }
