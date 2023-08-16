07c5b67a039f58514b96a131c5a6f3220cd71d00
Al7assan <al7assan.r@gmail.com>
2023-08-16 14:33:44 +0300

    c 0.1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * print_name - print a name.
@@ -9,5 +10,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
