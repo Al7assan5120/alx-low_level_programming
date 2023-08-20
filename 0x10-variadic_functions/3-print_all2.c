a435d6c2ff79aa459c92c7821eea0dc38d98d109
Al7assan <al7assan.r@gmail.com>
2023-08-20 19:31:51 +0300

    H 1

--- 0x10-variadic_functions/3-print_all2.c
+++ 0x10-variadic_functions/3-print_all2.c
@@ -15,12 +15,12 @@
  *       the character to be printed.
  */
 
-/*void print_char(va_list argu)
+void print_char(va_list argu)
 {
 	char c;
 	c = va_arg(argu, int);
 	printf("%c", c);
-}*/
+}
 
 /**
  * print_int - Prints an int.
@@ -28,12 +28,12 @@
  *       the integer to be printed.
  */
 
-/*void print_int(va_list argu)
+void print_int(va_list argu)
 {
 	int i;
 	i = va_arg(argu, int);
 	printf("%d", i);
-}*/
+}
 
 /**
  * print_float - Prints a float.
@@ -41,12 +41,12 @@
  *       the float to be printed.
  */
 
-/*void print_float(va_list argu)
+void print_float(va_list argu)
 {
 	float f;
 	f = va_arg(argu, double);
 	printf("%f", f);
-}*/
+}
 
 /**
  * print_string - Prints a string.
@@ -54,7 +54,7 @@
  *       the string to be printed.
  */
 
-/*void print_string(va_list argu)
+void print_string(va_list argu)
 {
 	char *s;
 	s = va_arg(argu, char*);
@@ -66,7 +66,7 @@
 	{
 		printf("%s", s);
 	}
-}*/
+}
 
 
 void print_all(const char * const format, ...)
@@ -74,40 +74,22 @@ void print_all(const char * const format, ...)
 	va_list args;
 	char *s;
 	int i = 0;
-	const char *p;
-	p = format;
 
 	va_start(args, format);
-	/*fun print[] = {
+	fun print[] = {
 		{"c", print_char},
 		{"i", print_int},
 		{"f", print_float},
 		{"s", print_string}
-	};*/
-	while (*p != '\0')
+	};
+	while (format && *(format + i) != '\0')
 	{
-		switch (*p)
+		i = 0;
+		while (*(format + i) == print[i].symbols)
 		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			s = va_arg(args, char*);
-			printf("%s",(s == NULL) ? "(nil" : s);
-			break;
+			print[i].f(args);
 		}
-		if (i < 3 && (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's'))
-		{
-			printf(", ");
-		}
-		i++;
-		p++;
+		
 	}
 	printf("\n");
 	va_end(args);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,11 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-/*typedef struct main
+typedef struct main
 {
     char symbols;
     void(*f)(va_list argu);
-}fun;*/
+}fun;
 void print_char(va_list argu);
 void print_int(va_list argu);
 void print_float(va_list argu);
