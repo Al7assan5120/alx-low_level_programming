7f6598ee4091062d7e7083e735406c87f9873be4
Al7assan <al7assan.r@gmail.com>
2023-08-25 04:07:12 +0300

    c 0.1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,20 @@ int print_list(const list_t *h)
 	const list_t *ptr = NULL;
 
 	ptr = h;
-	if (ptr == NULL)
-	{
-		printf("[0] (nil)");
-	}
 	while (ptr != NULL)
 	{
-		coun++;
-		printf("[%d] %s\n", ptr->len, ptr->str);
-		ptr = ptr->next;
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			coun++;
+			ptr = ptr->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+			coun++;
+			ptr = ptr->next;
+		}
 	}
 	return (coun);
 }
