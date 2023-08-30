fddfe2d703db78135a07d4c1d123975054e03afc
Al7assan <al7assan.r@gmail.com>
2023-08-30 04:36:55 +0300

    c 9.3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *front, *back;
 	listint_t *temp = malloc(sizeof(listint_t));
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return (NULL);
 	}
