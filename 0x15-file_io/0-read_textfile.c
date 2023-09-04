320cfc7f192207eed7e2b9c63dce73f7c610b4c6
Al7assan <al7assan.r@gmail.com>
2023-09-05 02:58:27 +0300

    c 0.3

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, wr, let;
+	int fd, wr, r;
 	char *t;
 
 	t = malloc(sizeof(char) * letters);
@@ -20,12 +20,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	let = read(fd, t, letters);
-	wr = write(STDOUT_FILENO, t, letters);
-	if (wr == -1 || let == -1)
+	r = read(fd, t, letters);
+	wr = write(STDOUT_FILENO, t, r);
+	if (wr == -1 || r == -1 || wr != r)
+	{
+		free(t);
 		return (0);
+	}
 
 	close(fd);
 	free(t);
-	return (let);
+	return (r);
 }
