cef11e94765d4902ed128117a281587c977f83ea
Al7assan <al7assan.r@gmail.com>
2023-09-05 03:26:43 +0300

    c 1.2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		w = write(fd, text_content, strlen(text_content));
 	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
