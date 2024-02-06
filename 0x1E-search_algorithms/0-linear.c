d35f4161e0a619e1b45edcd764e71f85b9dcb944
Al7assan <al7assan.r@gmail.com>
2024-02-06 23:51:07 +0200

    c 0.1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,6 +18,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%li] = [%i]\n", i, *array);
 		if (*array == value)
 		{
 			return (i);
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+*binary_search - fun. using binary_search algorithm.
+*@array: pointer to the array.
+*@size: the size of the array
+*@value: is the value to search for
+*Return: no return
+**/
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
