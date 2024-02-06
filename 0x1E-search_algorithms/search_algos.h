fdcb0985dc0d4f599ffb5d514279b332ae4c5a7e
Al7assan <al7assan.r@gmail.com>
2024-02-06 23:42:20 +0200

    c 0.0

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
