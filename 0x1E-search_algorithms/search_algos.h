3427f4a5c44704f37b7d5d368157d155e85d24d7
Al7assan <al7assan.r@gmail.com>
2024-02-07 01:44:41 +0200

    c 100.1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
